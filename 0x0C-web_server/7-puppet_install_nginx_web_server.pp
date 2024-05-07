494394471c9b6ccf46d047042fdb1439b6e6b325
gerry_bron <geraldduah1@gmail.com>
2024-05-07 16:53:21 +0000

    puppet setup update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,6 @@
-# Install Nginx package
-package { 'nginx':
-  ensure => installed,
-}
+# Installs a Nginx server
 
-# Create Hello World index page
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-}
-
-# Configure Nginx to perform 301 redirect for /redirect_me
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  content => template('nginx_redirect/default.erb'),
-  require => Package['nginx'],
-}
-
-# Enable default site by creating symbolic link
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => File['/etc/nginx/sites-enabled/default'],
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
