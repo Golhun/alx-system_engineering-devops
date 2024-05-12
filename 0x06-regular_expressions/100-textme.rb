159d8a9fb89db5c7d6abf0f5822cdccd6f179dbb
Gerald Adu-Broni <geraldduah1@gmail.com>
2024-05-12 18:33:49 +0000

    made files executable

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,8 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+# Extracts sender, receiver, and flags from a log file.
+# Output format: [SENDER],[RECEIVER],[FLAGS]
+# - Sender: phone number or name with country code
+# - Receiver: phone number or name with country code
+# - Flags: transaction flags used
+
+puts ARGV[0].scan(/\[from:(.*)\] \[to:(.*)\] \[flags:(.*?)\]/).join(",")
