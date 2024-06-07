c20000b56687771704dd31212e202251bd6b5b6a
Gerald Adu-Broni <geraldduah1@gmail.com>
2024-06-07 01:27:23 +0000

    new update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,39 @@
 #!/usr/bin/python3
-
 """
-prints the titles of the first 10 hot posts listed for a given subreddit
+Module to print the titles of the first 10 hot posts listed for a given subreddit.
 """
 
-from requests import get
+import requests
 
 
 def top_ten(subreddit):
     """
-    function that queries the Reddit API and prints the titles of the first
-    10 hot posts listed for a given subreddit
-    """
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
 
+    Args:
+        subreddit (str): The subreddit to query.
+    """
     if subreddit is None or not isinstance(subreddit, str):
         print("None")
+        return
 
     user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
     params = {'limit': 10}
     url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
+    response = requests.get(url, headers=user_agent, params=params)
+    if response.status_code != 200:
+        print("None")
+        return
+
     results = response.json()
 
     try:
-        my_data = results.get('data').get('children')
+        my_data = results.get('data', {}).get('children', [])
 
-        for i in my_data:
-            print(i.get('data').get('title'))
+        for post in my_data:
+            print(post.get('data', {}).get('title'))
 
     except Exception:
         print("None")
